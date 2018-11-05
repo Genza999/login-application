7aa084e997ba06164d8235b532d85a27bbd03af8
Genza999 <cartpix@gmail.com>
2018-11-05 20:23:51 +0330

    updated config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ class DevelopmentConfig(Config):
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = 'cartpix@gmail.com'
-    MAIL_PASSWORD = 'Countries9!'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
